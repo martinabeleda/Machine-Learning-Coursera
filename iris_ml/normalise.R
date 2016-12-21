83977603d4bfce85205c207854df091c9bb6a2bf
mabe6816 <mabe6816@uni.usyd.edu.au>
2016-12-21 15:34:24 +1000

    hi

--- iris_ml/normalise.R
+++
@@ -1,8 +0,0 @@
-# Normalise function
-# usage: YourNormalisedDataSet <- as.data.frame(lapply(YourDataSet, normalise))
-normalise <- function(x) {
-    num <- x - min(x)
-    denom <- max(x) - min(x)
-    return(num/denom)
-}
-
